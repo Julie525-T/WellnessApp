7b1d88e107876ddff77be082356edcea12f03e48
Jules525-T <jullytracy576@gmail.com>
2026-04-09 16:34:27 +0300

    I added the splash component and also uploaded test ads from admob in my project

--- app/src/main/java/com/julie/simba_wellnessapp/MainActivity.kt
+++ app/src/main/java/com/julie/simba_wellnessapp/MainActivity.kt
@@ -8,11 +8,42 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.MobileAds
+
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
+
 class MainActivity : AppCompatActivity() {
+
+//    A variable to store our interstitial ad
+    private var mInterstitialAd: InterstitialAd?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+//        mobileAds.initialize-preparing the app to load the app
+        MobileAds.initialize(this)
+
+//        Get the adview from layout using the ID
+        val adView=findViewById<AdView>(R.id.adView)
+
+//        Creare a request asking admob for an ad
+        val adRequest= AdRequest.Builder().build()
+
+//        Load the ad and send request to admob then admob returns the add
+        adView.loadAd(adRequest)
+
+//        Call the function to load the ad from the server
+
+        loadInterstitialAd()
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -30,6 +61,9 @@ class MainActivity : AppCompatActivity() {
 
             val recipesIntent= Intent(applicationContext, HealthyRecipes::class.java)
             startActivity(recipesIntent)
+
+//            Show your ad here
+            showInterstitialAd()
         }
 
 //        Nutrition intent
@@ -80,6 +114,34 @@ class MainActivity : AppCompatActivity() {
             val progressIntent= Intent(applicationContext, WeeklyGoals::class.java)
 
             startActivity(progressIntent)
+        }
+    }
+
+//    A fuction to load our interstitial ad from the server
+
+    fun loadInterstitialAd() {
+        val adRequest = AdRequest.Builder().build()
+
+        InterstitialAd.load(
+            this,
+            "ca-app-pub-3940256099942544/1033173712", // Test ID
+            adRequest,
+            object : InterstitialAdLoadCallback() {
+
+                override fun onAdLoaded(ad: InterstitialAd) {
+                    mInterstitialAd = ad
+                }
+
+                override fun onAdFailedToLoad(error: LoadAdError) {
+                    mInterstitialAd = null
+                }
+            }
+        )
+    }
+    //Show Interstitial ad
+    fun showInterstitialAd() {
+        if (mInterstitialAd != null) {
+            mInterstitialAd?.show(this)
         }
     }
 }
